549677c74c18be5f421fdefc9cceef49e84f3473
fokosun <okosunuzflorence@gmail.com>
2019-10-20 15:12:11 +0100

    CORS still blocking request to GH

--- web_client/index.js
+++ web_client/index.js
@@ -1,11 +1,40 @@
-// Import the express lirbary
 const express = require('express')
 
-// Create a new express application and use
-// the express static middleware, to serve all files
-// inside the public directory
-const app = express()
-app.use(express.static(__dirname + '/public'))
+// Import the axios library, to make HTTP requests
+const axios = require('axios')
+const cors = require('cors')
 
-// Start the server on port 8080
+// This is the client ID and client secret that you obtained
+// while registering the application
+const clientID = 'c7aae19cb2cf0733b019'
+const clientSecret = 'cb3094d64447c69f5e8931cf3cd91b0053dbdce2'
+
+const app = express()
+
+// Declare the redirect route
+app.get('/oauth/redirect', (req, res) => {
+  // The req.query object has the query params that
+  // were sent to this route. We want the `code` param
+  const requestToken = req.query.code
+  axios({
+    // make a POST request
+    method: 'post',
+    // to the Github authentication API, with the client ID, client secret
+    // and request token
+    url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
+    // Set the content type header, so that we get the response in JSOn
+    headers: {
+         accept: 'application/json'
+    }
+  }).then((response) => {
+    // Once we get the response, extract the access token from
+    // the response body
+    const accessToken = response.data.access_token
+    // redirect the user to the welcome page, along with the access token
+    res.redirect(`/welcome.html?access_token=${accessToken}`)
+  })
+})
+
+app.use(express.static(__dirname + '/public'))
+app.use(cors())
 app.listen(8080)
