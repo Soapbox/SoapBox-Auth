acbcf9e15c708cee9ebfa1539c77aa57aa107907
fokosun <okosunuzflorence@gmail.com>
2019-11-06 17:52:30 +0100

    set the response code

--- auth-service/app/Exceptions/UserNotFoundException.php
+++ auth-service/app/Exceptions/UserNotFoundException.php
@@ -3,5 +3,14 @@
 namespace App\Exceptions;
 
 use Exception;
+use Throwable;
+use Illuminate\Http\Response;
 
-class UserNotFoundException extends Exception {}
+class UserNotFoundException extends Exception
+{
+	public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
+	{
+		parent::__construct($message, $code, $previous);
+		$this->code = Response::HTTP_NOT_FOUND;
+	}
+}
