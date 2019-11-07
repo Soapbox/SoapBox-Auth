92277fd7c4ae0a257126ef5303fb5da54816f177
fokosun <okosunuzflorence@gmail.com>
2019-11-07 15:33:54 +0100

    wp

---
+++ auth-service/app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AuthController extends Controller
+{
+	public function logout(Request $request)
+	{
+		$this->validate($request, [
+			'jwt' => 'required'
+		]);
+
+		if(app('redis')->sIsMember(env('REDIS_KEY'), $request->jwt)){
+			app('redis')->sRem(env('REDIS_KEY'), $request->jwt);
+			$code = Response::HTTP_OK;
+		} else {
+			$code = Response::HTTP_UNAUTHORIZED;
+		}
+
+		return response(null, $code);
+	}
+}
