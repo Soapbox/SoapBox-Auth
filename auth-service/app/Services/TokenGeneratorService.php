25bd912e93b8177042d13c98518edbf4c6c9f529
fokosun <okosunuzflorence@gmail.com>
2019-11-12 16:37:34 +0100

    cleanup

--- auth-service/app/Services/TokenGeneratorService.php
+++ auth-service/app/Services/TokenGeneratorService.php
@@ -7,17 +7,15 @@ use App\Libraries\iJWTLibrary;
 use Illuminate\Http\Response;
 use Laravel\Socialite\Facades\Socialite;
 use App\Exceptions\UserNotFoundException;
-use Laravel\Socialite\Two\User as SocialProviderUser;
 
 class TokenGeneratorService
 {
-	protected $key;
-	protected $exp;
 	protected $code; //refers to oauth_code from provider
 	protected $token;
 	protected $payload;
 	protected $provider; //ex google, slack, microsoft
 	protected $jwt_library;
+	protected $socialProviderUser;
 
 	const ISS = "http://auth-server.test";
 	const AUD = "http://api-gateway.test";
@@ -37,12 +35,12 @@ class TokenGeneratorService
 		$this->setProvider(Arr::get($args, 'provider'));
 		$this->setCode(Arr::get($args, 'code'));
 
-		$socialProviderUser = Socialite::driver($this->provider)->userFromToken($this->code);
+		$this->socialProviderUser = Socialite::driver($this->provider)->userFromToken($this->code);
 
 		$userExist = true; //assumption
 
 		if ($userExist) {
-			$this->payload = $this->generatePayload($socialProviderUser);
+			$this->payload = $this->generatePayload();
 			$this->token = $this->jwt_library->encode($this->payload);
 		} else {
 			throw new UserNotFoundException('User not found.: ' . $socialProviderUser->getEmail(), Response::HTTP_NOT_FOUND);
@@ -53,18 +51,17 @@ class TokenGeneratorService
 
 	/**
 	 * Generate payload
-	 * @param SocialProviderUser $user
 	 * @return array
 	 */
-	protected function generatePayload(SocialProviderUser $user): array
+	protected function generatePayload(): array
 	{
 		return array(
 			"iss" => self::ISS,
 			"aud" => self::AUD,
 			"iat" => time(),
 			"exp" => strtotime('+1 '. $this->jwt_library->getExpiry()),
-			"name" => $user->getName(),
-			"email" => $user->getEmail()
+			"name" => $this->socialProviderUser->getName(),
+			"email" => $this->socialProviderUser->getEmail()
 		);
 	}
 
