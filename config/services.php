42889abb95949c9804c31b8174788813ecf12ec8
Laravel Shift <shift@laravelshift.com>
2021-06-03 21:23:41 +0000

    Shift folder structure
    Laravel 5 has a new folder structure. This commit moves folders to
    their new locations in an effort to preserve their version history.

--- config/services.php
+++ config/services.php
@@ -1,19 +1,33 @@
 <?php
 
 return [
-    'google' => [
-        'client_id' => env('GOOGLE_CLIENT_ID'),
-        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => null,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
     ],
-    'slack' => [
-        'client_id' => env('SLACK_KEY'),
-        'client_secret' => env('SLACK_SECRET'),
-        'redirect' => null,
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
     ],
-    'graph' => [
-        'client_id' => env('GRAPH_KEY'),
-        'client_secret' => env('GRAPH_SECRET'),
-        'redirect' => null,
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+
 ];
